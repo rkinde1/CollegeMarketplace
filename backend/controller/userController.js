db86592a6114db6a03593370218493f6f444f6ff
rkinde1 <rosekinder2020@gmail.com>
2023-10-06 16:40:24 -0400

    Working database

---
+++ backend/controller/userController.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const User = require('../models/userModel');
+
+//error here
+const register = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    if (await User.findOne({email})) return res.status(400).json({ message: 'User already exists' });
+    try {
+        const newUser = await User.create({ firstName, lastName, email, password });
+        await newUser.save();
+        res.status(200).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Error creating user' });
+    }
+}
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'User does not exist' });
+    if (user.password !== password) return res.status(400).json({ message: 'Incorrect password' });
+    res.status(200).json({ message: 'Success' });
+}
+
+module.exports = { register, login };
---
+++ frontend/src/Signup.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Signup () {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        //needs validation
+        //Insert Connor's function to verify email and password
+        await fetch('/api/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ firstName, lastName, email, password }),
+        })
+        .then((res) => {
+            if (res.status === 200) {
+                alert('Success');
+                console.log('Success');
+                return res.json();
+            } else {
+                alert('Failed');
+                alert(res.status);
+                console.log('Failed');
+            }
+        })
+    }
+    return(
+        <div>
+            <form onSubmit={handleSignup} method="POST"> 
+                <h1>Sign Up</h1>
+                <label htmlFor="firstName">First Name: </label>
+                <input type="text" id="firstName" onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+                <br></br>
+                <label htmlFor="lastName">Last Name: </label>
+                <input type="text" id="lastName" onChange={(e) => setLastName(e.target.value)} value={lastName} />
+                <br></br>
+                <label htmlFor="email">Email: </label>
+                <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+                <br></br>
+                <label htmlFor="password">Password: </label>
+                <input type="password" id="password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                <br></br>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    );
+}
+
+export default Signup;
