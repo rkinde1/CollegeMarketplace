50609ba0b58d58f3bde9e6e333a85f3602d97a50
Matt Burton <mburton112@gmail.com>
2023-10-06 20:36:18 -0400

    Removed legacy signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 //import Home from './Home';
 import Signup from './Signup';
-import Login from './Login';
+import Login from './login';
 import ForgotPassword from './ForgotPassword';
 import ResetPassword from './ResetPassword';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -15,7 +15,7 @@ function App() {
           <Routes>
             {/* <Route path="/" element={<Home />}></Route> */}
             <Route path="/register" element={<Signup />}></Route>
-            <Route path="/login" element={<Login />}></Route>
+            <Route path="/login" element={<login />}></Route>
             <Route path="/forgot-password" element={<ForgotPassword />}></Route>
             <Route path="/reset_password" element={<ResetPassword />}></Route> 
           </Routes>
@@ -23,18 +23,6 @@ function App() {
       </div>
     </BrowserRouter>
   )
-}
-
-function App() {
-  return (
-    <div className="App">
-    <header className="App-header">
-        {/* Add the Signup component */}
-        <Signup />
-        <Login />
-    </header>
-    </div>
-  );
 }
 
 export default App;
