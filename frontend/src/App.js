a471567966bf0fb9cf034337bd2920fc59b48bd9
rkinde1 <rosekinder2020@gmail.com>
2023-10-08 22:32:11 -0400

    Navbar done and now signup only works for towson emails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 //import Home from './Home';
+import 'bootstrap/dist/css/bootstrap.css';
 import Signup from './Signup';
-import Login from './Login';
+import Login from './login';
 import ForgotPassword from './ForgotPassword';
 import ResetPassword from './ResetPassword';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Navbar, NavLink, Nav, Container} from 'react-bootstrap';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter forceRefresh={true}>
+      <Navbar>
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Nav>
+          <NavLink href="/register">Register</NavLink>
+          <NavLink href="/login">Login</NavLink>
+          <NavLink href="/forgot-password">Forgot Password</NavLink>
+          <NavLink href="/reset_password">Reset Password</NavLink>
+        </Nav>
+      </Navbar>
       <div className="App">
         <header className="App-header">
           <Routes>
@@ -20,6 +31,7 @@ function App() {
             <Route path="/forgot-password" element={<ForgotPassword />}></Route>
             <Route path="/reset_password" element={<ResetPassword />}></Route> 
           </Routes>
+
         </header>
       </div>
     </BrowserRouter>
