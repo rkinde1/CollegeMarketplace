a1c9d051eac766ba80da9da0ec13eb2de1a97aac
jeldri2 <jeldri2@students.towson.edu>
2023-10-10 16:19:44 -0400

    Added homepage

---
+++ frontend/src/homepage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Home = ()=>{
+    return(
+        <div>
+            <section id = "Home">
+                <div className = "container">
+                    <div className ="row justify-content-center"> 
+                        <div className = "col-md-8 mt-5">
+                            <h1 className = "display-4 fw-bolder mb-4 text-center">Example text1
+                            </h1>
+                            <p className = "lead text-center fs-4 mb-5" >
+                                Example text 2
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default Home
