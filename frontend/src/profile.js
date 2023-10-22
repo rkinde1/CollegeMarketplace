33d5a8c8f31cf7196afb14feaa3f54c64da7388b
Connor Brown <connorbrown0220@gmail.com>
2023-10-22 00:41:37 -0400

    preped profile for comments

--- frontend/src/profile.js
+++ frontend/src/profile.js
@@ -2,9 +2,32 @@ import React, {useEffect, useState} from "react"
 import { useNavigate, Link} from "react-router-dom"
 import Popup from "reactjs-popup";
 import { Button } from 'react-bootstrap';
-import history from './history';
+import uploadImage from "./UploadImage"
+
 
 function Profile () {
+    //const [comments, setComments] = useState([]);
+
+    // const handleComments = (e) => {
+    //     fetch('/api/comments/view', {
+    //         method: 'GET',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //         },
+    //     })
+    //     .then((res) => {
+    //         if (res.status === 200) {
+    //             console.log('Success');
+    //             return res.json();
+    //         } else {
+    //             console.log('Failed');
+    //         }
+    //     })
+    //     .then((data) => {
+    //         // alert(JSON.stringify(data));
+    //         setComments(data);
+    //     })
+    // }
 
     //const userImg = localStorage.getItem('icon');
     const navigate = useNavigate();
@@ -13,70 +36,47 @@ function Profile () {
     const firstName =localStorage.getItem('firstName');
     const imageLocation = localStorage.getItem('defaultImage');
 
-    async function uploadImage(file) { // file from <input type="file"> 
-        // const data = new FormData();
-        // data.append("file", file);
-        // data.append("upload_preset", CollegeMarketPlace);
-      
-        // const res = await fetch(
-        //   `https://api.cloudinary.com/v1_1/${'34ecccb5bf9c0877db2edf49666747'}/image/upload`,
-        //   {
-        //     method: "POST",
-        //     body: data,
-        //   }
-        // );
-        // const img = await res.json();
-        // // Post `img.secure_url` to your server and save to MongoDB
-      }
-
-      function send(){
-        navigate('/UploadImage');
-      }
     const divStyle = {
         backgroundImage: `url('${imageLocation}')`,
         backgroundSize: 'cover',
         height: '480px',
         paddingTop: '80px',
       };
+
+      const navigateToUploadImage = () => {
+        // 👇️ navigate to /UploadImage
+        navigate('/UploadImage');
+      };
+    
+
     const imgStyle = {
         height: '150px',
     }
     return(
         
         <div>
-            {/* switch to users name */}
-            <h1> {firstName} </h1><br/>
+            {/* switch to users name and add rating */}
+            <h1> {firstName}  </h1><br/>
             <div style={divStyle}>
-                <img src={imageLocation} style={imgStyle} alt="Description" />
+                <img src={imageLocation} style={imgStyle} alt="Profile Picture Here" />
             </div>
-                <form>
-                    <Link to = "/UploadImage">Upload Profile Picture</Link>
-                </form>
+            <button onClick={navigateToUploadImage}>Upload Profile Picture</button>
+                
+             {/*<button onClick={handleComments}>View Comments</button>
+            <div className="comment-list">
+            {comments.map((comment) => (
+                <div className="comment" key={comment._id}>
+                    <h2>{comment.commentName}</h2>
+                    <p>{comment.commentTitle}</p>
+                    <p>{comment.commentDescription}</p>
+                    <p>{comment.commentDate}</p>
+                    <p>{comment.commentRating}</p>
+                    <p className="userFont">{comment.date}</p>
+                </div>   
+                ))}
+            </div> */}
         </div>
     )
 }
 
 export default Profile;
-
-// const handleImageChange = (e) =>{
-
-//     fetch('/api/signup', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ image }),
-//     })
-//     .then((res) => {
-//         if (res.status === 200) {
-//             alert('Success');
-//             console.log('Success');
-//             return res.json();
-//         }
-//         else {
-//             alert('Failed');
-//             alert(res.status);
-//             console.log('Failed');
-//         }
-//     })
-// }
